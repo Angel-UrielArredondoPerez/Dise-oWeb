88055f9f2a0fef5378249da7cff1de2ef08a75f4
Angel-UrielArredondoPerez <127697194+Angel-UrielArredondoPerez@users.noreply.github.com>
2024-04-24 20:22:21 -0600

    Pagina web
    No contiene los archivos js y ccs por que no me permitía pero esta completa la pagina hasta ahorita , faltan algunos cambios desde la parte de frond - end

---
+++ Pagina web/include/navbar.php
@@ -0,0 +1,46 @@
+<section id="navegacion">
+      <nav class="navbar navbar-expand-xl navbar-light fixed-top" style="background-color: rgba(128, 0, 58, 0.822);">
+        <div class="container-fluid">
+          <a class="navbar-brand text-white" href="#" style="color: rgb(128, 7, 45);">DIGITAL CHERRY'S   </a>
+          <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active text-white menuprincipal" aria-current="page" href="index.php">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-dash-fill" viewBox="0 0 16 16">
+  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
+  <path d="m8 3.293 4.712 4.712A4.5 4.5 0 0 0 8.758 15H3.5A1.5 1.5 0 0 1 2 13.5V9.293z"/>
+  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M11 12h3a.5.5 0 0 1 0 1h-3a.5.5 0 1 1 0-1"/>
+</svg>
+                  <svg class="bi" width="28" height="28" fill="currentColor">
+                    <use xlink:href="library/icons/bootstrap-icons.svg#heart-fill" style="font-family: 'Times New Roman', Times, serif;" />
+                  </svg> INICIO</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white menuprincipal" href="somos.php" style="font-family:'Times New Roman', Times, serif;"  >QUIENES SOMOS</a> 
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white menuprincipal" href="Servicios.php" style="font-family: 'Times New Roman', Times, serif;">SERVICIOS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white menuprincipal" href="Proyectos.php" style="font-family: 'Times New Roman', Times, serif;">PROYECTOS</a>
+              </li>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white menuprincipal" href="Contacto.php" style="font-family: 'Times New Roman', Times, serif;">CONTACTANOS</a>
+              </li>
+            </ul>
+            <span class="navbar-text">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link text-white menuprincipal" href="#" style="font-family:'Times New Roman', Times, serif;">AYUDA</a>
+                </li>
+            </span>
+  
+          </div>
+        </div>
+      </nav>
+    </section>
